f9b30688c60c7057d8cceed88d64145a63d6a6a7
TeamDoodz <unconfigured@null.spigotmc.org>
2022-09-06 13:58:54 -0500

    better input handling

--- HuskVR/MonoBehaviours/VRInputManager.cs
+++ HuskVR/MonoBehaviours/VRInputManager.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace HuskVR.MonoBehaviours {
+	/*
 	[ConfigureSingleton(SingletonFlags.PersistAutoInstance)]
 	public class VRInputManager : MonoSingleton<VRInputManager> {
+		// there is a much more elegant way to do this but whatevs
 		private void Update() {
+			if(VRInput.MockInput == null) {
+				VRInput.MockInput = VRInput.SetUpMockInputForActions(InputManager.Instance.bindings[0].Action.actionMap.asset);
+			}
+			if(VRInput.MockInput == null) {
+				MainPlugin.logger.LogError("MockInput is null.");
+				return;
+			}
 			if(VRInput.IsRightHandTriggerDown) {
 				Fire();
 			}
 		}
 		private void Fire() {
+			
 			if(GunControl.Instance == null) return;
 			{
 				RocketLauncher rocketLauncher = GunControl.Instance.currentWeapon.GetComponent<RocketLauncher>();
@@ -26,4 +38,5 @@ namespace HuskVR.MonoBehaviours {
 			}
 		}
 	}
+*/
 }
--- HuskVR/VRInput.cs
+++ HuskVR/VRInput.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using System.Text;
 using HuskVR.MonoBehaviours;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Layouts;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR;
 
 namespace HuskVR {
 	public static class VRInput {
-		private static InputDevice? headset;
-		private static InputDevice? leftHand;
-		private static InputDevice? rightHand;
+		private static UnityEngine.XR.InputDevice? headset;
+		private static UnityEngine.XR.InputDevice? leftHand;
+		private static UnityEngine.XR.InputDevice? rightHand;
 
-		public static InputDevice Headset => headset ?? throw new Exception("Headset is null.");
-		public static InputDevice LeftHand => leftHand ?? throw new Exception("Left Hand is null.");
-		public static InputDevice RightHand => rightHand ?? throw new Exception("Right Hand is null.");
+		public static UnityEngine.XR.InputDevice Headset => headset ?? throw new Exception("Headset is null.");
+		public static UnityEngine.XR.InputDevice LeftHand => leftHand ?? throw new Exception("Left Hand is null.");
+		public static UnityEngine.XR.InputDevice RightHand => rightHand ?? throw new Exception("Right Hand is null.");
 
 		public static bool IsRightHandTriggerDown {
 			get {
-				if(RightHand.TryGetFeatureValue(CommonUsages.triggerButton, out bool outp)) {
+				if(RightHand.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out bool outp)) {
 					return outp;
 				} else {
 					MainPlugin.logger.LogWarning("Could not read triggerButton usage of Right Hand");
@@ -26,12 +28,22 @@ namespace HuskVR {
 				}
 			}
 		}
+		public static bool IsRightHandBDown {
+			get {
+				if(RightHand.TryGetFeatureValue(UnityEngine.XR.CommonUsages.secondaryButton, out bool outp)) {
+					return outp;
+				} else {
+					MainPlugin.logger.LogWarning("Could not read secondaryButton usage of Right Hand");
+					return false;
+				}
+			}
+		}
 
-		static List<InputDevice> inputDevices = new List<InputDevice>();
+		static List<UnityEngine.XR.InputDevice> inputDevices = new List<UnityEngine.XR.InputDevice>();
 		internal static void InitInput() {
 			MainPlugin.logger.LogMessage("Initializing input");
 
-			inputDevices = new List<InputDevice>();
+			inputDevices = new List<UnityEngine.XR.InputDevice>();
 			InputDevices.GetDevices(inputDevices);
 
 			if(inputDevices == null) {
@@ -45,18 +57,12 @@ namespace HuskVR {
 
 			InputDevices.deviceConnected += InitDevice;
 
-			SceneManager.activeSceneChanged += (Scene a, Scene b) => {
-				if(VRInputManager.Instance == null) {
-					new GameObject("VR Input Manager").AddComponent<VRInputManager>();
-				}
-			};
-
 			if(headset == null) MainPlugin.logger.LogWarning("Could not find headset. Make sure it exists and is connected to the system.");
 			if(leftHand == null) MainPlugin.logger.LogWarning("Could not find left hand. Make sure it exists and is connected to the system.");
 			if(rightHand == null) MainPlugin.logger.LogWarning("Could not find right hand. Make sure it exists and is connected to the system.");
 		}
 
-		private static void InitDevice(InputDevice device) {
+		private static void InitDevice(UnityEngine.XR.InputDevice device) {
 			MainPlugin.logger.LogInfo(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics));
 			if(device.characteristics.HasFlag(InputDeviceCharacteristics.HeadMounted)) {
 				headset = device;
