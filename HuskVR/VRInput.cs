3fd5300a6df5eb247cdcb5c3a83ff2cf969c2c26
TeamDoodz <unconfigured@null.spigotmc.org>
2022-09-06 12:45:49 -0500

    use events for getting devices

--- HuskVR/VRInput.cs
+++ HuskVR/VRInput.cs
@@ -38,22 +38,28 @@ namespace HuskVR {
 			}
 
 			foreach(var device in inputDevices) {
-				MainPlugin.logger.LogInfo(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics));
-				if(device.characteristics.HasFlag(InputDeviceCharacteristics.HeadMounted)) {
-					headset = device;
-				} else if(device.characteristics.HasFlag(InputDeviceCharacteristics.Controller)) {
-					if(device.characteristics.HasFlag(InputDeviceCharacteristics.Left)) {
-						leftHand = device;
-					}
-					if(device.characteristics.HasFlag(InputDeviceCharacteristics.Right)) {
-						rightHand = device;
-					}
-				}
+				InitDevice(device);
 			}
 
-			if(headset == null) MainPlugin.logger.LogError("Could not find headset. Make sure it exists and is connected to the system.");
-			if(leftHand == null) MainPlugin.logger.LogError("Could not find left hand. Make sure it exists and is connected to the system.");
-			if(rightHand == null) MainPlugin.logger.LogError("Could not find right hand. Make sure it exists and is connected to the system.");
+			InputDevices.deviceConnected += InitDevice;
+
+			if(headset == null) MainPlugin.logger.LogWarning("Could not find headset. Make sure it exists and is connected to the system.");
+			if(leftHand == null) MainPlugin.logger.LogWarning("Could not find left hand. Make sure it exists and is connected to the system.");
+			if(rightHand == null) MainPlugin.logger.LogWarning("Could not find right hand. Make sure it exists and is connected to the system.");
+		}
+
+		private static void InitDevice(InputDevice device) {
+			MainPlugin.logger.LogInfo(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics));
+			if(device.characteristics.HasFlag(InputDeviceCharacteristics.HeadMounted)) {
+				headset = device;
+			} else if(device.characteristics.HasFlag(InputDeviceCharacteristics.Controller)) {
+				if(device.characteristics.HasFlag(InputDeviceCharacteristics.Right)) {
+					rightHand = device;
+				}
+				if(device.characteristics.HasFlag(InputDeviceCharacteristics.Left)) {
+					leftHand = device;
+				}
+			}
 		}
 	}
 }
