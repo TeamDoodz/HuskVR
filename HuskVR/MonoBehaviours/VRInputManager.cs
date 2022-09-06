892baa67bfb4d06a7f7c54aeaf21f8f990cd3f10
TeamDoodz <unconfigured@null.spigotmc.org>
2022-09-06 12:30:10 -0500

    shoot rockets with right trigger

---
+++ HuskVR/MonoBehaviours/VRInputManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HuskVR.MonoBehaviours {
+	[ConfigureSingleton(SingletonFlags.PersistAutoInstance)]
+	public class VRInputManager : MonoSingleton<VRInputManager> {
+		private void Update() {
+			if(VRInput.IsRightHandTriggerDown) {
+				Fire();
+			}
+		}
+		private void Fire() {
+			if(GunControl.Instance == null) return;
+			{
+				RocketLauncher rocketLauncher = GunControl.Instance.currentWeapon.GetComponent<RocketLauncher>();
+				if(rocketLauncher != null) {
+					if(!rocketLauncher.wid || rocketLauncher.wid.delay == 0f) {
+						rocketLauncher.Shoot();
+						return;
+					}
+					rocketLauncher.Invoke("Shoot", rocketLauncher.wid.delay);
+					rocketLauncher.cooldown = 1f;
+				}
+			}
+		}
+	}
+}
