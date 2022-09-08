d38c94a0d195d32b688f394912f526cc3205f546
Waffle <60797216+wafflethings@users.noreply.github.com>
2022-09-08 22:09:44 +0100

    fix rot

---
+++ HuskVR/Patches/RotPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
+
+namespace HuskVR.Patches {
+	public class RotPlayer
+	{
+		//im sorry this is a hack
+
+		static GameObject camera;
+		static GameObject holder;
+
+		[HarmonyPatch(typeof(NewMovement), nameof(NewMovement.Update))]
+		static class RotPlayer_Update
+		{
+			static void Postfix(NewMovement __instance)
+			{
+				if (camera != null)
+				{
+					__instance.transform.rotation = Quaternion.Euler(0, camera.transform.rotation.eulerAngles.y, 0);
+					holder.transform.rotation = Quaternion.Euler(0, 0, 0);
+					camera.transform.position = new Vector3(0, camera.transform.position.y, 0);
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(NewMovement), nameof(NewMovement.Start))]
+		static class RotPlayer_Start
+		{
+			static void Postfix(NewMovement __instance)
+			{
+				__instance.StartCoroutine(WaitAndParent(__instance));
+			}
+		}
+
+		static IEnumerator WaitAndParent(NewMovement __instance)
+        {
+			yield return new WaitForSeconds(1);
+
+			holder = new GameObject("Camera Holder");
+			holder.transform.parent = __instance.gameObject.transform;
+
+			camera = __instance.gameObject.ChildByName("Main Camera");
+			holder.transform.localPosition = Vector3.zero;
+			camera.transform.parent = holder.transform;
+			camera.transform.localPosition = Vector3.zero;
+		}
+	}
+}
---
+++ HuskVR/UnityUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace HuskVR
+{
+    public static class UnityUtils
+    {
+        public static GameObject ChildByName(this GameObject from, string name)
+        {
+            List<GameObject> children = new List<GameObject>();
+            int count = 0;
+            while (count < from.transform.childCount)
+            {
+                children.Add(from.transform.GetChild(count).gameObject);
+                count++;
+            }
+
+            if (count == 0)
+            {
+                return null;
+            }
+
+            for (int i = 0; i < children.Count; i++)
+            {
+                if (children[i].name == name)
+                {
+                    return children[i];
+                }
+            }
+            return null;
+        }
+    }
+}
