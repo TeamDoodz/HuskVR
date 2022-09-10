4abf319e25b1908953281373d24f848ec470429f
TeamDoodz <unconfigured@null.spigotmc.org>
2022-09-10 16:01:40 -0500

    better style hud

--- HuskVR/Patches/MoveHUDPatch.cs
+++ HuskVR/Patches/MoveHUDPatch.cs
@@ -9,6 +9,9 @@ namespace HuskVR.Patches {
 			if(__instance.gameObject.name == "GunCanvas") {
 				__instance.transform.localScale = new UnityEngine.Vector3(0.001f, 0.001f, 0.001f);
 				__instance.defaultPos = new UnityEngine.Vector3(-0.75f, -0.4f, 1.6f);
+			} else if(__instance.gameObject.name == "StyleCanvas") {
+				__instance.transform.localScale = new UnityEngine.Vector3(0.002f, 0.002f, 0.001f);
+				__instance.defaultPos = new UnityEngine.Vector3(1.3f, 0.2f, 1.6f);
 			}
 		}
 	}
