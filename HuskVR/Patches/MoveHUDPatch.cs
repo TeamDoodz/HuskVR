11627b867559286e7de58efae901a7cf61f14a49
TeamDoodz <unconfigured@null.spigotmc.org>
2022-09-10 15:48:50 -0500

    better hud

---
+++ HuskVR/Patches/MoveHUDPatch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using  HarmonyLib;
+
+namespace HuskVR.Patches {
+	[HarmonyPatch(typeof(HUDPos), nameof(HUDPos.Start))]
+	static class MoveHUDPatch {
+		static void Prefix(HUDPos __instance) {
+			if(__instance.gameObject.name == "GunCanvas") {
+				__instance.transform.localScale = new UnityEngine.Vector3(0.001f, 0.001f, 0.001f);
+				__instance.defaultPos = new UnityEngine.Vector3(-0.75f, -0.4f, 1.6f);
+			}
+		}
+	}
+}
