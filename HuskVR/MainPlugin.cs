c211f4ba395175fd98fabf528fe1c45f0dc7e3fc
TeamDoodz <unconfigured@null.spigotmc.org>
2022-10-13 23:20:20 -0500

    versioning stuff

--- HuskVR/MainPlugin.cs
+++ HuskVR/MainPlugin.cs
@@ -1,29 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
 
 namespace HuskVR {
-	[BepInPlugin(GUID,Name,Version)]
+	[BepInPlugin(GUID,Name,VersionString)]
 	public class MainPlugin : BaseUnityPlugin {
 
 		public const string GUID = "io.github.TeamDoodz.HuskVR";
 		public const string Name = "HuskVR";
-		public const string Version = "0.0.1";
+		private const string VersionString = "0.0.1";
+
+		public static Version Version { get; } = new Version(VersionString);
+		public static string BuildConfiguration { get; } = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyConfigurationAttribute>().Configuration;
 
 		internal static ManualLogSource logger { get; private set; }
 
 		private void Awake() {
 			logger = Logger;
 
+			VersionChecker.CheckVersion();
+
 			new Harmony(GUID).PatchAll();
 
 			VRInput.InitInput();
 			VRUI.InitUI();
 
-			logger.LogMessage($"{Name} version {Version} loaded!");
+			logger.LogMessage($"{Name} version {BuildConfiguration} {VersionString} loaded!");
 		}
 
 		private void Update() {
@@ -32,5 +38,5 @@ namespace HuskVR {
 			}
 		}
 
-	}
+    }
 }
---
+++ HuskVR/VersionChecker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HuskVR {
+	public static class VersionChecker {
+		const string VERSION_URI = "https://api.github.com/repos/TeamDoodz/HuskVR/tags";
+
+		public static Version LatestVersion { get; private set; }
+
+		private static string Get(string uri) {
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+			request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+			using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+			using(Stream stream = response.GetResponseStream())
+			using(StreamReader reader = new StreamReader(stream)) {
+				string responseString = reader.ReadToEnd();
+				MainPlugin.logger.LogDebug($"Request: {uri}");
+				MainPlugin.logger.LogDebug($"Response:\n{responseString}");
+				return responseString;
+			}
+		}
+
+		internal static void CheckVersion() {
+			VersionTag[] versions = JsonConvert.DeserializeObject<VersionTag[]>(Get(VERSION_URI));
+			if(versions.Length == 0) {
+				MainPlugin.logger.LogDebug("HuskVR has no public releases");
+			}
+			if(versions[0].Name > MainPlugin.Version) {
+				MainPlugin.logger.LogWarning($"HuskVR outdated. Current version: {MainPlugin.Version}; Latest version: {LatestVersion}");
+			}
+		}
+
+		private struct VersionTag {
+			[JsonProperty("name", ItemConverterType = typeof(Newtonsoft.Json.Converters.VersionConverter))]
+			public Version Name;
+		}
+	}
+}
