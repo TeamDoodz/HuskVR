a6b6fe170d568b956a7c80740443d2b1c72a80ce
TeamDoodz <unconfigured@null.spigotmc.org>
2022-10-15 16:07:34 -0500

    preloader patcher maybe??????

---
+++ HuskVR.Patcher/MainPatcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Logging;
+using Mono.Cecil;
+
+namespace HuskVR.Patcher {
+	public static class MainPatcher {
+		private static ManualLogSource logger = new ManualLogSource("HuskVR.Patcher");
+
+		public static IEnumerable<string> TargetDLLs { get; } = new string[0];
+
+		public static void Patch(AssemblyDefinition ad) {
+		}
+	}
+}
